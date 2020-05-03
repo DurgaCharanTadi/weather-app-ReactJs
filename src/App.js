b46ebb76e655ac90cdec7b9ad2780ca7d9443a5d
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2020-05-03 14:55:55 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, This is Weather app made in ReactJS</h1>
+      <div className="header">
+        <img src={require('./images/logo.png')} alt="My Logo" height="100" width="140"/>
+          <ul className="menuItems">
+              <li>My Projects</li>
+              <li>About</li>
+          </ul>
+      </div>
+
+      <div className="content">
+        <h2>This is a Weather Application designed in ReactJS</h2>
+      </div>
     </div>
   );
 }
