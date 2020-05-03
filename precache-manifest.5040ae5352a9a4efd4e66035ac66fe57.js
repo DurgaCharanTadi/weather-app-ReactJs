b8874c1ee8735d5bbee812998d890a59ee4affcd
Durga Charan Tadi <durgacharan.tinku@gmail.com>
2020-05-03 01:16:27 -0700

    Updates

---
+++ precache-manifest.5040ae5352a9a4efd4e66035ac66fe57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6aa11adbc33138c27695f7a7d1904c6",
+    "url": "/weather-app-reactjs/index.html"
+  },
+  {
+    "revision": "5da9fd5da7bced119468",
+    "url": "/weather-app-reactjs/static/css/main.7b07a5cf.chunk.css"
+  },
+  {
+    "revision": "afcde0701f001acfd06f",
+    "url": "/weather-app-reactjs/static/js/2.9db2f094.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-reactjs/static/js/2.9db2f094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da9fd5da7bced119468",
+    "url": "/weather-app-reactjs/static/js/main.604b51f0.chunk.js"
+  },
+  {
+    "revision": "3a0db74b92e8e52949ff",
+    "url": "/weather-app-reactjs/static/js/runtime-main.a49ec8f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
